28626619e6e9d693474bbedd2188b0486fc1fc00
vishul786 <vishulk786@gmail.com>
2024-03-07 01:21:12 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,39 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from 'cookie-parser'
+
+import authRoutes from "./routes/auth.routes.js"
+import messageRoutes from "./routes/message.routes.js"
+import userRoutes from "./routes/user.routes.js"
+
+import connectToMongoDB from "./db/connectToMongoDB.js";
+import bodyParser from "body-parser";
+
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+dotenv.config();
+
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
+
+//app.use(express.json());    // to parse the incoming request with JSON payloads from (req.body())
+
+// app.get('/',(req,res) => {
+//     //root route http://localhost:5000/
+//     res.send("Hello World!!");
+// });
+
+
+app.listen(PORT,() => {
+    connectToMongoDB();
+    console.log(`Server Running on port ${PORT}`);
+});
+
+
+
